7dec7def47e56554e0b9b94dfea8bc066f0c5da5
cristina <cristina.outeda.rua@gmail.com>
2022-04-10 01:21:12 +0200

    removed console.log

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -3,17 +3,17 @@ import { StyledCard } from "./Card.styled";
 import { Text } from "../globals";
 
 export const Card = ({ phone }: any) => {
-  const priceHelper = (price: string) => {
+  const priceHelper = () => {
     const priceHelper = phone.price.replace(/About /g, "");
-    console.log({ priceHelper });
     return priceHelper;
   };
+
   return (
     <StyledCard>
       <img className="card-image" src={phone.thumbnailFileName} alt="phone" />
       <div className="card-bottom">
         <Text secondary text={phone.name} />
-        <Text bold text={priceHelper(phone.price)} />
+        <Text bold text={priceHelper()} />
       </div>
     </StyledCard>
   );
