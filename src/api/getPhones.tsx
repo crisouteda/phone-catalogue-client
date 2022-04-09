9d6fce3d29f7fd4b52516538c652da2f2b82bcba
cristina <cristina.outeda.rua@gmail.com>
2022-04-09 21:14:38 +0200

    simplified arrow function

--- src/api/getPhones.tsx
+++ src/api/getPhones.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { Config } from "../services";
 
-export const getPhones = async () => {
-  return await axios.get(`${Config.apiBase}phones/getPhones`);
-};
+export const getPhones = async () =>
+  await axios.get(`${Config.apiBase}phones/getPhones`);
