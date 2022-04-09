42a776f55d60c01df86a6b0353fb5ea48457335e
cristina <cristina.outeda.rua@gmail.com>
2022-04-10 01:19:12 +0200

    renamed file and added endpoint

--- src/api/endpoints.tsx
+++ src/api/endpoints.tsx
@@ -1,5 +1,8 @@
 import axios from "axios";
 import { Config } from "../services";
 
+export const getPhone = async (id: string) =>
+  await axios.get(`${Config.apiBase}phones/phone/${id}`);
+
 export const getPhones = async () =>
   await axios.get(`${Config.apiBase}phones/phones`);
