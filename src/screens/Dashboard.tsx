ffaba2fb722c2bd45a7f0ab33c4aee2a03cdf893
cristina <cristina.outeda.rua@gmail.com>
2022-04-09 21:01:59 +0200

    added get phones function to page

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Header } from "../components";
+import { useContextActions } from "../context";
+import { useContextState } from "../context";
 
 export default function Dashboard() {
+  const { handleGetPhones } = useContextActions();
+  const { getPhones, getPhonesLoading, getPhonesError } = useContextState();
+
+  useEffect(() => {
+    handleGetPhones();
+  }, []);
+
   return (
     <>
       <Header title="Phone Catalogue App" />
+      {getPhonesLoading && <span>Loading...</span>}
+      {console.log({ getPhones })}
     </>
   );
 }
