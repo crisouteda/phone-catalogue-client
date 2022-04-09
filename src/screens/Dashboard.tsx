d0242a4e85f29475f74430d261abe5b36af98395
cristina <cristina.outeda.rua@gmail.com>
2022-04-09 15:44:01 +0200

    created dashboard as a screen

---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Header } from "../components";
+
+export default function Dashboard() {
+  return (
+    <>
+      <Header title="Phone Catalogue App" />
+    </>
+  );
+}
