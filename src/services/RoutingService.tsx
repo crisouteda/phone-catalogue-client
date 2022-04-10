ffa4f84901b2bf6984d92927ac20c6eee2c8e798
cristina <cristina.outeda.rua@gmail.com>
2022-04-10 21:39:06 +0200

    removed unused route

--- src/services/RoutingService.tsx
+++ src/services/RoutingService.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Dashboard from "../screens/Dashboard";
-import { PhoneModal } from "../components/phoneModal";
 
 export const RoutingService = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/:id" element={<PhoneModal />} />
       </Routes>
     </BrowserRouter>
   );
