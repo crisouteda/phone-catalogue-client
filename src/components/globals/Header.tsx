df4768036bff0e2d381f76d69807baae071aba0e
cristina <cristina.outeda.rua@gmail.com>
2022-04-14 00:15:32 +0200

    added sing up/in button to header

--- src/components/globals/Header.tsx
+++ src/components/globals/Header.tsx
@@ -1,6 +1,10 @@
 import React, { memo } from "react";
 import { StyledHeader } from "./Header.styled";
+import { PrimaryButton } from "./DashboardComponents";
 
 export const Header = memo(({ title }: { title: string }) => (
-  <StyledHeader>{title}</StyledHeader>
+  <StyledHeader>
+    {title}
+    <PrimaryButton text={"Sign Up"} alignSelf="flex-end" />
+  </StyledHeader>
 ));
