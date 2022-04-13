da3e96f158f90ef3c256b4984c7cf290935a91ce
cristina <cristina.outeda.rua@gmail.com>
2022-04-14 00:11:27 +0200

    renamed file

--- src/api/mainEndpoints.tsx
+++ src/api/mainEndpoints.tsx
@@ -8,7 +8,8 @@ export const getPhone = async (id: string) => {
 
 export const getPhones = async (items: number, lastScanned?: string) => {
   const request = await fetch(
-    `${Config.apiBase}phones/pagination/${items}/${lastScanned}`
+    // `${Config.apiBase}phones/pagination/${items}/${lastScanned}`
+    `${Config.apiBase}phones`
   );
   const response = await request.json();
   return response;
