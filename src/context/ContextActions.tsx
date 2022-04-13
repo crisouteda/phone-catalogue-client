5b45c49853aed80e45a7e626d1a70b3080743cc5
cristina <cristina.outeda.rua@gmail.com>
2022-04-14 00:15:07 +0200

    added auth actions to context

--- src/context/ContextActions.tsx
+++ src/context/ContextActions.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { getPhones, getPhone } from "../api";
+import { getPhones, getPhone, signUp, signIn, isUserAuth } from "../api";
 import {
   GET_PHONES,
   GET_PHONES_SUCCESS,
@@ -8,12 +8,24 @@ import {
   GET_PHONE_SUCCESS,
   GET_PHONE_FAIL,
   CLEAR_PHONE,
+  SING_UP,
+  SIGN_UP_SUCCESS,
+  SIGN_UP_FAIL,
+  SING_IN,
+  SIGN_IN_FAIL,
+  SIGN_IN_SUCCESS,
+  IS_AUTH,
+  IS_AUTH_SUCCESS,
+  IS_AUTH_FAIL,
 } from "./actionTypes";
 
 export interface iActions {
   handleGetPhones: any;
   handleGetPhone: any;
   handleClearPhone: any;
+  handleSignUp: any;
+  handleSignIn: any;
+  handleIsSign: any;
 }
 
 function handleGetPhones(dispatch: React.Dispatch<any>) {
@@ -33,7 +45,6 @@ function handleGetPhone(dispatch: React.Dispatch<any>) {
     dispatch({ type: GET_PHONE });
     try {
       const response = await getPhone(id);
-      console.log({ response });
       dispatch({ type: GET_PHONE_SUCCESS, payload: response[0] });
     } catch (e) {
       dispatch({ type: GET_PHONE_FAIL, payload: e });
@@ -47,10 +58,49 @@ function handleClearPhone(dispatch: React.Dispatch<any>) {
   };
 }
 
+function handleSignUp(dispatch: React.Dispatch<any>) {
+  return async function handleGetPhones(email: string, password: string) {
+    dispatch({ type: SING_UP });
+    try {
+      const response = await signUp(email, password);
+      dispatch({ type: SIGN_UP_SUCCESS, payload: response });
+    } catch (e) {
+      dispatch({ type: SIGN_UP_FAIL, payload: e });
+    }
+  };
+}
+
+function handleSignIn(dispatch: React.Dispatch<any>) {
+  return async function handleGetPhone(email: string, password: string) {
+    dispatch({ type: SING_IN });
+    try {
+      const response = await signIn(email, password);
+      dispatch({ type: SIGN_IN_SUCCESS, payload: response });
+    } catch (e) {
+      dispatch({ type: SIGN_IN_FAIL, payload: e });
+    }
+  };
+}
+
+function handleIsSign(dispatch: React.Dispatch<any>) {
+  return async function handleGetPhone(email: string, password: string) {
+    dispatch({ type: IS_AUTH });
+    try {
+      const response = await isUserAuth();
+      dispatch({ type: IS_AUTH_SUCCESS, payload: response });
+    } catch (e) {
+      dispatch({ type: IS_AUTH_FAIL, payload: e });
+    }
+  };
+}
+
 const functions = {
   handleGetPhones,
   handleGetPhone,
   handleClearPhone,
+  handleSignUp,
+  handleSignIn,
+  handleIsSign,
 };
 
 export default functions;
--- src/context/actionTypes.tsx
+++ src/context/actionTypes.tsx
@@ -1,3 +1,16 @@
+// auth context
+export const SING_UP = "SIGN_UP";
+export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
+export const SIGN_UP_FAIL = "SIGN_UP_FAIL";
+
+export const SING_IN = "SIGN_IN";
+export const SIGN_IN_SUCCESS = "SIGN_IN_SUCESS";
+export const SIGN_IN_FAIL = "SIGN_IN_FAIL";
+
+export const IS_AUTH = "IS_AUTH";
+export const IS_AUTH_SUCCESS = "IS_AUTH_SUCCESS";
+export const IS_AUTH_FAIL = "IS_AUTH_FAIL";
+
 // phones context
 export const GET_PHONES = "GET_PHONES";
 export const GET_PHONES_SUCCESS = "GET_PHONES_SUCCESS";
