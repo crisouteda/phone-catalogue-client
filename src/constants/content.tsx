69cc4ac4f41d75296428ba5bf3d991ce8268a11f
cristina <cristina.outeda.rua@gmail.com>
2022-04-18 11:59:01 +0200

    added test for signup and sign in

--- src/constants/content.tsx
+++ src/constants/content.tsx
@@ -10,6 +10,9 @@ export const ENABLE_EDIT_BUTTON = "Edit information";
 export const DELETE_ITEM_BUTTON = "Delete";
 export const UPDATE_ITEM_BUTTON = "Update";
 export const CREATE_PHONE_BUTTON = "Add phone";
+export const SEND_AUTH_DATA_BUTTON = "Finalize";
+export const FROM_SIGNUP_TO_SIGNIN_TEXT = "Already have an account";
+export const FROM_SIGNIN_TO_SIGNUP_TEXT = "Back to register";
 
 export const SIGN_UP_BUTTON = "Sign Up";
 export const LOG_OUT_BUTTON = "Log Out";
