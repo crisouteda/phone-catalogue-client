f66e65d6565834bf4dbbbdc86a32d4a98d2bcae3
cristina <cristina.outeda.rua@gmail.com>
2022-04-18 11:07:55 +0200

    added and styled toggle button

--- src/components/globals/GlobalComponents.styled.tsx
+++ src/components/globals/GlobalComponents.styled.tsx
@@ -163,3 +163,63 @@ export const CustomLoadingDots = styled.div`
     background: ${(props) => props.theme.secondary};
   }
 `;
+
+export const CustomSwitch = styled.label`
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+
+  .switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: ${(props) => props.theme.bg_accent};
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
+
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
+
+  input:checked + .slider {
+    background-color: ${(props) => props.theme.bg};
+  }
+
+  input:focus + .slider {
+    box-shadow: 0 0 1px ${(props) => props.theme.bg};
+  }
+
+  input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
+
+  /* Rounded sliders */
+  .slider.round {
+    border-radius: 34px;
+  }
+
+  .slider.round:before {
+    border-radius: 50%;
+  }
+`;
